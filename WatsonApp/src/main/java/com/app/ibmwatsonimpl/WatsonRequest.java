5ae3be45d40470ed6e71ac7853b4b717ab58e96a
felix.ojiem <oriedofelo@gmail.com>
2017-05-11 11:45:55 +0300

    Look at Watson Natural Language Processing

--- WatsonApp/src/main/java/com/app/ibmwatsonimpl/WatsonRequest.java
+++ WatsonApp/src/main/java/com/app/ibmwatsonimpl/WatsonRequest.java
@@ -6,17 +6,27 @@ import java.util.Map;
 import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
 import com.ibm.watson.developer_cloud.alchemy.v1.model.DocumentSentiment;
 import com.ibm.watson.developer_cloud.http.ServiceCall;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalysisResults;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalyzeOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.EntitiesOptions;
+import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Features;
 
 public class WatsonRequest {
 
 	public static void main(String[] args) {
-		AlchemyLanguage service = new AlchemyLanguage();
-		service.setApiKey("<api_key>");
-		Map<String,Object> params = new HashMap<String, Object>();
-		params.put(AlchemyLanguage.TEXT, "IBM Watson won the Jeopardy television show hosted by Alex Trebek");
-		ServiceCall<DocumentSentiment> sentiment =  service.getSentiment(params);
-		sentiment.execute();
-		System.out.println(sentiment);
+		NaturalLanguageUnderstanding service;
+		service = new NaturalLanguageUnderstanding(NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27);
+		service.setUsernameAndPassword("e56c4fb7-21a7-4199-84f9-0666e7a121ab",
+				"5lEZ3Vo0kiVA");
+		service.setEndPoint("https://gateway.watsonplatform.net/natural-language-understanding/api");
+		EntitiesOptions entities = new EntitiesOptions.Builder().sentiment(true).emotion(true).limit(5).build();
+		Features features = new Features.Builder().entities(entities).build();
+		AnalyzeOptions parameters = new AnalyzeOptions.Builder().text("Who won USA elections 2016?").features(features).build();
+		AnalysisResults results = service.analyze(parameters).execute();
+		System.err.println(results.getAnalyzedText());
+		
+		
 
 	}
 
